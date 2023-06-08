c85fe751def86e8afe5b403726bb7ffbe8e71d68
sedhadcci <125959892+sedhadcci@users.noreply.github.com>
2023-06-08 17:28:31 +0200

    Update python.py

--- python.py
+++ python.py
@@ -7,14 +7,16 @@ def load_data(file):
     df = pd.read_excel(file)
     return df
 
-def search_data(df, query, columns):
+def search_data(df, query, column='Compétences'):
     df_copy = df.copy()  # Crée une copie du DataFrame
     results = []
-    for col in columns:
-        df['Match_Score'] = df[col].apply(lambda row: process.extractOne(query, [str(row)], score_cutoff=70))
-        matched_df = df[df['Match_Score'].notna()]
+    keywords = [word.strip() for word in query.split(',')]
+    for keyword in keywords:
+        df_copy['Match_Score'] = df_copy[column].apply(lambda row: process.extractOne(keyword, [str(row)], score_cutoff=70))
+        matched_df = df_copy[df_copy['Match_Score'].notna()]
         for i, row in matched_df.iterrows():
-            results.append(row['Resultat'])
+            result = {'Domaine': row['Domaine'], 'Nom formation': row['Nom formation'], 'Ecoles': row['Ecoles'], 'Resultat': row['Resultat']}
+            results.append(result)
     return results
 
 st.title("Moteur de recherche EXCEL")
@@ -22,10 +24,9 @@ st.title("Moteur de recherche EXCEL")
 uploaded_file = st.file_uploader("Choisir un fichier Excel", type="xlsx")
 if uploaded_file is not None:
     df = load_data(uploaded_file)
-    query = st.text_input("Entrer le(s) mot(s) clé(s)")
+    query = st.text_input("Entrer le(s) mot(s) clé(s) séparés par des virgules")
     if query:
         st.subheader("Résultats de recherche")
-        columns = ["Compétences", "Domaine", "Nom formation", "Ecoles"]
-        results = search_data(df, query, columns)
+        results = search_data(df, query)
         for result in results:
             st.write(result)
