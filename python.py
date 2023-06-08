472922f468598345c1ffe143bd0302c0845aab0c
sedhadcci <125959892+sedhadcci@users.noreply.github.com>
2023-06-08 17:54:27 +0200

    Update python.py

--- python.py
+++ python.py
@@ -10,9 +10,9 @@ def load_data(file):
 def search_data(df, query, column='Compétences'):
     df_copy = df.copy()  # Crée une copie du DataFrame
     results = []
-    keywords = [word.strip() for word in query.split(',')]
-    for keyword in keywords:
-        df_copy['Match_Score'] = df_copy[column].apply(lambda row: process.extractOne(keyword, [str(row)], score_cutoff=70))
+    phrases = [phrase.strip() for phrase in query.split(',')]
+    for phrase in phrases:
+        df_copy['Match_Score'] = df_copy[column].apply(lambda row: process.extractOne(phrase, [str(row)], score_cutoff=70))
         matched_df = df_copy[df_copy['Match_Score'].notna()]
         for i, row in matched_df.iterrows():
             result = {'Domaine': row['Domaine'], 'Nom formation': row['Nom formation'], 'Ecoles': row['Ecoles'], 'Resultat': row['Resultat']}
@@ -24,7 +24,7 @@ st.title("Moteur de recherche EXCEL")
 uploaded_file = st.file_uploader("Choisir un fichier Excel", type="xlsx")
 if uploaded_file is not None:
     df = load_data(uploaded_file)
-    query = st.text_input("Entrer le(s) mot(s) clé(s) séparés par des virgules")
+    query = st.text_input("Entrer le(s) phrase(s) à rechercher, séparées par des virgules")
     if query:
         st.subheader("Résultats de recherche")
         results = search_data(df, query)
