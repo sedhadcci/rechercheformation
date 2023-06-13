a863975ca7bf750b7d7a35f97ac6bedf69a03002
sedhadcci <125959892+sedhadcci@users.noreply.github.com>
2023-06-13 10:55:14 +0200

    Update python.py

--- python.py
+++ python.py
@@ -10,9 +10,9 @@ def load_data(file):
 def search_data(df, query, column='Mots clés compétences'):
     df_copy = df.copy()  # Crée une copie du DataFrame
     results = []
-    phrases = [phrase.strip() for phrase in query.split(',')]
+    phrases = [phrase.strip().lower() for phrase in query.split(',')]  # Convertit chaque phrase en minuscules
     for phrase in phrases:
-        df_copy['Match_Score'] = df_copy[column].apply(lambda row: fuzz.partial_ratio(phrase, str(row)) if fuzz.partial_ratio(phrase, str(row)) >= 70 else 0)
+        df_copy['Match_Score'] = df_copy[column].apply(lambda row: fuzz.partial_ratio(phrase, str(row).lower()) if fuzz.partial_ratio(phrase, str(row).lower()) >= 70 else 0)  # Compare les phrases et les lignes en minuscules
         matched_df = df_copy[df_copy['Match_Score'] > 0]
         for i, row in matched_df.iterrows():
             result = {'Ecoles': row['Ecoles'], 'Filières / domaine': row['Filières / domaine'], 'Formation': row['Formation'], 'Poste': row['Poste'], 'Lien': row['Lien'], 'TYPE': row['TYPE']}
