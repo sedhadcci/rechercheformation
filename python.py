2341cd79b60cafd840af6d49b6531c79579b769f
sedhadcci <125959892+sedhadcci@users.noreply.github.com>
2023-06-15 16:00:15 +0200

    Update python.py

--- python.py
+++ python.py
@@ -18,14 +18,12 @@ st.title("Recherche de formation")
 
 choice = st.radio(
     "Choisissez le fichier sur lequel vous voulez effectuer la recherche",
-    ('ISTEC', 'Groupe éducatif CCI', 'Ferrandi'))  # Ajout de 'Ferrandi' comme troisième choix.
+    ('ISTEC', 'Groupe éducatif CCI'))
 
 if choice == 'ISTEC':
     url = 'https://raw.githubusercontent.com/sedhadcci/rechercheformation/main/ISTEC%20recherche%20formation.xlsx'
 elif choice == 'Groupe éducatif CCI':
     url = 'https://raw.githubusercontent.com/sedhadcci/rechercheformation/main/Groupe%20%C3%A9ducatif%20CCI%20moteur%20de%20recherche%20formation.xlsx'
-elif choice == 'Ferrandi':  # Ajout du lien pour le fichier Ferrandi.
-    url = 'https://raw.githubusercontent.com/sedhadcci/rechercheformation/main/Ferrandi%20mots%20cles.xlsx'
 
 df = load_data(url)
 
